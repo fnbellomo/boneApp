ef7541a26161eafef078fe12b825034d13e73fbe
fnbellomo <fnbellomo@gmail.com>
2015-01-08 03:00:24 -0300

    initial commint

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# boneApp
+# Bone App
+
+#HowTo install Bower and the JS libraries (in Debian)
+
+1. First, we need install [Node.JS](https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager). Like sudo, we do:
+
+        # apt-get install curl
+        # curl -sL https://deb.nodesource.com/setup | bash -
+        # apt-get install -y nodejs
+
+2. Now, install [Bower](http://bower.io/):
+
+        $ sudo npm install -g bower
---
+++ wsgi/drive_method.py
@@ -0,0 +1,58 @@
+from apiclient import errors
+from apiclient.http import MediaFileUpload
+
+def insert_file(service, title, description, parent_id, mime_type, filename):
+  """Insert new file.
+
+  Args:
+    service: Drive API service instance.
+    title: Title of the file to insert, including the extension.
+    description: Description of the file to insert.
+    parent_id: Parent folder's ID.
+    mime_type: MIME type of the file to insert.
+    filename: Filename of the file to insert.
+
+  Returns:
+    Inserted file metadata if successful, None otherwise.
+
+  Other:
+	HTTP request: POST
+  """
+  media_body = MediaFileUpload(filename, mimetype=mime_type, resumable=True)
+  body = {
+    'title': title,
+    'description': description,
+    'mimeType': mime_type
+  }
+  # Set the parent folder.
+  if parent_id:
+    body['parents'] = [{'id': parent_id}]
+
+  try:
+    file = service.files().insert(
+        body=body,
+        media_body=media_body).execute()
+
+    # Uncomment the following line to print the File ID
+    # print 'File ID: %s' % file['id']
+
+    return file
+  except errors.HttpError, error:
+    print 'An error occured: %s' % error
+    return None
+
+
+def delete_file(service, file_id):
+  """Permanently delete a file, skipping the trash.
+
+  Args:
+    service: Drive API service instance.
+    file_id: ID of the file to delete.
+
+  Other:
+	HTTP request: DELETE
+  """
+  try:
+    service.files().delete(fileId=file_id).execute()
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
