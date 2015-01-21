83d58ca73c92c436ed81824158ad9de3419a5a48
fnbellomo <fnbellomo@gmail.com>
2015-01-21 16:59:34 -0300

    howto install and configuration

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Bone App
 
-#HowTo install Bower and the JS libraries (in Debian)
+##HowTo install Bower and the JS libraries (in Debian)
 
 1. First, we need install [Node.JS](https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager). Like sudo, we do:
 
@@ -11,3 +11,23 @@
 2. Now, install [Bower](http://bower.io/):
 
         $ sudo npm install -g bower
+
+##Install Python dependence
+
+	$ HACER EL requerimets.txt
+    $ pip install requerimets.txt
+
+##Enable the Drive API
+
+Usar como guia para escribir este link: https://developers.google.com/drive/web/quickstart/quickstart-python
+
+1. Go to the [Google Developers Console](https://console.developers.google.com/).
+2. Select a project, or create a new one.
+3. In the sidebar on the left, expand **APIs & auth**. Next, click **APIs**. In the list of APIs, make sure the status is **ON** for the *Drive API* and *Drive SDK*.
+4. In the sidebar on the left, select **Credentials**.
+    1. In either case, you end up on the **Credentials** page and can create your project's credentials from here.
+    2. If you haven't done so already, create your OAuth 2.0 credentials by clicking **Create new Client ID** under the **OAuth** heading. Next, look for your application's client ID and client secret in the relevant table. You may also create and edit redirect URIs from this page.
+
+5. In **wsgi/route.py** remplace **YOUR_CLIENT_ID** with the **Client ID** and **YOUR_CLIENT_SECRET** with your **Client Secret**, both generated previously.
+
+TENGO QUE ACLARAR LO DE REDIRECT_URI, QUE TUBE QUE AGREGAR A MI DNS PARA QUE FUNCIONE.
--- wsgi/route.py
+++ wsgi/route.py
@@ -1,15 +1,25 @@
 from wsgi import app
 import json
 
-#Flask imports
+# Flask imports
 from flask import render_template, session, request, redirect, url_for
 import httplib2
 
-#To use Google APIs
-from apiclient.discovery import build
-#Para autentificar
+# To use Google APIs
+from apiclient import discovery
+# To authenticate with Google via OAuth2
 from oauth2client import client
 from oauth2client.client import OAuth2WebServerFlow
+
+# Copy your credentials from the console
+CLIENT_ID = "711876238709-jchulvq9841p5qiqghufp2tsd0s8bv12.apps.googleusercontent.com"
+CLIENT_SECRET = 'AR-6HkfkNKujajKgujRjxnQc'
+
+# Check https://developers.google.com/drive/scopes for all available scopes
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+
+# Redirect URI for installed apps
+REDIRECT_URI = 'http://a.b.com:8000/oauth2callback'
 
 @app.route('/')
 def home():
@@ -29,4 +39,29 @@ def maps():
 
 @app.route('/example/google')
 def google():
-  return render_template('google.html')
+  if 'credentials' not in session:
+    return redirect(url_for('oauth2callback'))
+  credentials = client.OAuth2Credentials.from_json(session['credentials'])
+  if credentials.access_token_expired:
+    return redirect(url_for('oauth2callback'))
+  else:
+    http_auth = credentials.authorize(httplib2.Http())
+    drive_service = discovery.build('drive', 'v2', http_auth)
+    files = drive_service.files().list().execute()
+    return json.dumps(files)
+
+@app.route('/oauth2callback')
+def oauth2callback():
+  flow = OAuth2WebServerFlow(client_id=CLIENT_ID,
+    client_secret = CLIENT_SECRET,
+    scope = OAUTH_SCOPE,
+    redirect_uri = REDIRECT_URI)
+  if 'code' not in request.args:
+    auth_uri = flow.step1_get_authorize_url()
+    return redirect(auth_uri)
+  else:
+    auth_code = request.args.get('code')
+    credentials = flow.step2_exchange(auth_code)
+    session['credentials'] = credentials.to_json()
+    return redirect(url_for('google'))
+
